65740df0d20a2ed8b324e327df9d1fcdb490e9d5
Philip Bradley <pdbradley@gmail.com>
2015-11-01 17:36:51 -0500

    fixed query to check correct value

--- spec/query_spec.rb
+++ spec/query_spec.rb
@@ -13,15 +13,19 @@ describe "Finding absent associations" do
 
   it "finds students with no teachers" do
     aclass = Schoolclass.create(name: "Fourth Period")
+    bclass = Schoolclass.create(name: "Fifth Period")
     s1 = Student.create(name: "Phil")
     s2 = Student.create(name: "Dan")
     t1 = Teacher.create(name: "Mcgillicudy")
 
     aclass.students << s1
     aclass.teachers << t1
+    bclass.students << s2
+
+    #s2 has a class, but no teachers
 
     expect(Student.includes(:teachers).
-           where(class_memberships: {student_id: nil})).to eq [s2]
+           where(timeslots: {teacher_id: nil})).to eq [s2]
 
   end
 
